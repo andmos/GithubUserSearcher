93f4d2cfa985d01064d6d260a368bc4ec47c9b12
Andreas Mosti <andreas.mosti@gmail.com>
2014-11-14 20:03:15 +0100

    added renderer for circle-images

---
+++ GithubUserSearcher/Controls/CircleImage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace GithubUserSearcher.Contols
+{
+	public class CircleImage : Image
+	{
+		public static readonly BindableProperty FileSourceProperty =
+			BindableProperty.Create<CircleImage, string>(p => p.FileSource, "");
+
+		/// <summary>
+		/// string filename
+		/// </summary>
+		/// <value>The source.</value>
+		public string FileSource
+		{
+			get { return (string)base.GetValue(FileSourceProperty);}
+			set { base.SetValue(FileSourceProperty, value);}
+		}
+
+		public static readonly BindableProperty BorderColorProperty =
+			BindableProperty.Create<CircleImage, Color>(p => p.BorderColor, Color.White);
+
+		public Color BorderColor
+		{
+			get { return (Color)base.GetValue(BorderColorProperty);}
+			set { base.SetValue(BorderColorProperty, value);}
+		}
+
+		public static readonly BindableProperty HasBorderProperty =
+			BindableProperty.Create<CircleImage, bool>(p => p.HasBorder, false);
+
+		public bool HasBorder
+		{
+			get { return (bool)base.GetValue(HasBorderProperty);}
+			set { base.SetValue(HasBorderProperty, value);}
+		}
+	}
+}
---
+++ iOS/Controls/CircleImageRenderer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Xamarin.Forms;
+using MonoTouch.UIKit;
+using Xamarin.Forms.Platform.iOS;
+using System.Drawing;
+using GithubUserSearcher.Contols;
+using GithubUsersSearcher.iOS.Controls;
+
+[assembly: ExportRendererAttribute(typeof(CircleImage), typeof(CircleImageRenderer))]
+namespace GithubUsersSearcher.iOS.Controls
+{
+
+	public class CircleImageRenderer : ViewRenderer<CircleImage,UIView>
+	{
+		UIImageView imageView;
+		protected override void OnElementChanged(ElementChangedEventArgs<CircleImage> e)
+		{
+			base.OnElementChanged(e);
+
+			var rbv = e.NewElement;
+			if (rbv != null) {
+
+				var mainView = new UIView();
+
+				imageView = new UIImageView (UIImage.FromBundle(rbv.FileSource));
+				imageView.Frame = new RectangleF 
+					(0, 0, (float)rbv.WidthRequest, (float)rbv.HeightRequest);
+				imageView.Layer.CornerRadius = imageView.Frame.Size.Width / 2;
+				if (rbv.HasBorder) {
+					imageView.Layer.BorderColor = rbv.BorderColor.ToCGColor();
+					imageView.Layer.BorderWidth = 2;
+				}
+				imageView.ClipsToBounds = true;
+
+				mainView.Add (imageView);
+
+				SetNativeControl(mainView);
+			}
+		}
+
+		protected override void OnElementPropertyChanged
+		(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+			if (e.PropertyName == CircleImage.HasBorderProperty.PropertyName) {
+				if (Element.HasBorder) {
+					imageView.Layer.BorderWidth = 2;
+					imageView.Layer.BorderColor = this.Element.BorderColor.ToCGColor ();
+				} else {
+					imageView.Layer.BorderWidth = 0;
+				}
+			}
+		}
+	}
+}
